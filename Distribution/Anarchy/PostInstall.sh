0deba045cce1719913394bdc8bde3c03bd9e323d
Evan Washkow <epwashkow@gmail.com>
2021-06-26 12:41:08 -0700

    refactor: move post install libs to variable

--- Distribution/Anarchy/PostInstall.sh
+++ Distribution/Anarchy/PostInstall.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+libs=$(pacman -Qe | grep "^lib")
 sudo pacman -Rns --noconfirm \
     adobe-source-code-pro-fonts \
     alsa-card-profiles \
     alsa-lib \
     alsa-topology-conf \
     alsa-ucm-conf \
-    $(pacman -Qe | grep "^lib") \
+    $libs \
     vim \
     zsh-syntax-highlighting
