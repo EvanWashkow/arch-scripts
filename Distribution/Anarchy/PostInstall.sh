e300bacd02cd2994038a3d479916c40068a15d2c
Evan Washkow <epwashkow@gmail.com>
2021-06-26 12:07:32 -0700

    feat: remove extra packages from Anarchy

--- Distribution/Anarchy/PostInstall.sh
+++ Distribution/Anarchy/PostInstall.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sudo pacman -Rns --noconfirm vim zsh-syntax-highlighting
+sudo pacman -Rns --noconfirm \
+    adobe-source-code-pro-fonts \
+    alsa-card-profiles \
+    alsa-lib \
+    alsa-topology-conf \
+    alsa-ucm-conf \
+    vim \
+    zsh-syntax-highlighting
