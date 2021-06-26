7b040aa6f6f6e47901eea72969744e38d3b0adb3
Evan Washkow <epwashkow@gmail.com>
2021-06-26 12:42:22 -0700

    refactor: remove libs after explicit packages

--- Distribution/Anarchy/PostInstall.sh
+++ Distribution/Anarchy/PostInstall.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-libs=$(pacman -Qe | grep "^lib")
-sudo pacman -Rns --noconfirm \
+sudo pacman -Rns \
     adobe-source-code-pro-fonts \
     alsa-card-profiles \
     alsa-lib \
     alsa-topology-conf \
     alsa-ucm-conf \
-    $libs \
     vim \
     zsh-syntax-highlighting
+sudo pacman -Rns $(pacman -Qe | grep "^lib")
