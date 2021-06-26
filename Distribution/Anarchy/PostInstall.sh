f1088fa44bec8896d3f0689ca29c22b0ee9372a9
Evan Washkow <epwashkow@gmail.com>
2021-06-26 12:42:22 -0700

    Revert "refactor: remove libs after explicit packages"
    This reverts commit 7b040aa6f6f6e47901eea72969744e38d3b0adb3.

--- Distribution/Anarchy/PostInstall.sh
+++ Distribution/Anarchy/PostInstall.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-sudo pacman -Rns \
+libs=$(pacman -Qe | grep "^lib")
+sudo pacman -Rns --noconfirm \
     adobe-source-code-pro-fonts \
     alsa-card-profiles \
     alsa-lib \
     alsa-topology-conf \
     alsa-ucm-conf \
+    $libs \
     vim \
     zsh-syntax-highlighting
-sudo pacman -Rns $(pacman -Qe | grep "^lib")
