a627ddab5c8798113e6d0c37464e43a48272c40b
Evan Washkow <epwashkow@gmail.com>
2021-12-29 21:57:33 -0800

    Install optional Nvidia dependencies

--- Graphics/InstallNvidia.sh
+++ Graphics/InstallNvidia.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# https://wiki.archlinux.org/title/NVIDIA
 # https://wiki.archlinux.org/title/xorg#Driver_installation
 
 sudo pacman -S --needed nvidia
+sudo pacman -D --asdeps nvidia-settings opencl-nvidia xorg-server-devel
